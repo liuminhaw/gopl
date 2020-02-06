21368191c67e59fb6dd75cc445b8fd33d93dceb5
Min-Haw, Liu <liuminhaw@gmail.com>
2020-02-06 22:20:28 +0800

    update: server1, server2, and server3

---
+++ ch1/server1/server1.go
@@ -0,0 +1,19 @@
+// Server1 is a minimal "echo" server.package server1
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler) // Each request calls handler
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+// handler echoes the path component of the requested URL
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
